77def2613ed32c879441b97d34880d682fa3a538
Am4nzi <45455994+Am4nzi@users.noreply.github.com>
2025-09-24 18:15:21 +0100

    update favicon

--- app/app.vue
+++ app/app.vue
@@ -6,3 +6,18 @@
     </NuxtLayout>
   </UApp>
 </template>
+
+<script setup lang="ts">
+const colorMode = useColorMode();
+import { useFavicon } from "@vueuse/core";
+
+const icon = useFavicon();
+
+onMounted(() => {
+  if (colorMode.preference === "light") {
+    icon.value = "/favicon-light.ico";
+  } else {
+    icon.value = "/favicon-dark.ico";
+  }
+});
+</script>
